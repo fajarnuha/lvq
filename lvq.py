f38035b7d635e0ba4e4eb89230cf4eb0c61f0301
fajar <fajarunuha@gmail.com>
2016-11-19 21:20:00 +0700

    change a bit

--- lvq.py
+++ lvq.py
@@ -1,15 +1,15 @@
 import numpy as np
-import random
 import math
+import time
 
 def error(i, w):
-    sum = np.sum(np.subtract(i,w)**2)
-    return math.sqrt(sum)
+    return math.sqrt(np.sum(np.subtract(i,w)**2))
 
 train = np.array([[1,3,0], [3,4,0], [6,1,1], [8,3,1], [9,1,1], [1,6,0]], dtype=np.float64)
 weights = np.array([[3,1],[7,4]], dtype=np.float64)
 learningRate = 0.5
 
+start = time()
 for input in train:
     minimVal = float('inf')
     minimInx = -1
@@ -24,4 +24,6 @@ for input in train:
         weights[minimInx] = weights[minimInx] - learningRate*(input[:2] - weights[minimInx])
     #learningRate = 0.1*learningRate
 
+spent = time() - start
+print spent
 print weights
